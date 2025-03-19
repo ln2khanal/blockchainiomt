b1beaf15002be587abcbf8deab0ef1314ebb70a0
Lekh Nath Khanal <ln2khanal@gmail.com>
2025-03-19 14:01:22 +0545

    adds settings option for blockchain storage

--- Vitals Watch App/SettingsView/SettingsView.swift
+++ Vitals Watch App/SettingsView/SettingsView.swift
@@ -10,8 +10,10 @@ import SwiftUI
 struct SettingsView: View {
     @AppStorage("useRealData") private var useRealData: Bool = false
     @AppStorage("hashingAlgorithm") private var hashingAlgorithm: Int = 256
+    @AppStorage("blockchainStorageScheme") private var blockchainStorageScheme: String = "Memory"
 
     let hasingAlgorighms = [256, 512]
+    let storageSchemes: [String] = ["Memory", "Local", "External"]
 
     var body: some View {
         Form {
@@ -20,6 +22,11 @@ struct SettingsView: View {
             Picker("Hashing Algorithm", selection: $hashingAlgorithm) {
                 ForEach(hasingAlgorighms, id: \.self) { algo in
                     Text("SHA-\(algo)").tag(algo)
+                }
+            }
+            Picker("Block Storage Scheme", selection: $blockchainStorageScheme) {
+                ForEach(storageSchemes, id: \.self) { scheme in
+                    Text(scheme).tag(scheme)
                 }
             }
         }
